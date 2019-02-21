73d059b9090f0f1d2e0fc2d153164466df169c31
Henry Panora <hpanora@gmail.com>
2019-02-21 03:55:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,14 @@ def countdown (number)
 end
 puts "Happy New Year!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} second(s)!"
+    number -= 1
+end
+puts "Happy New Year!"
+end
+
+countdown(11)
+countdown_with_sleep(3)
