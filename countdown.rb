97071c256179bc9de4b445082d30e11882d74aaa
Henry Panora <hpanora@gmail.com>
2019-02-21 03:56:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,15 @@
-#write your code here
-
-def countdown (number)
-  while number > 0
+def countdown (counter)
+  while counter > 0
     puts "#{number} second(s)!"
-    number -= 1
+    counter -= 1
 end
 puts "Happy New Year!"
 end
 
-def countdown_with_sleep(number)
-  while number > 0
-    puts "#{number} second(s)!"
-    number -= 1
+def countdown_with_sleep(counter)
+  while counter > 0
+    puts "#{counter} second(s)!"
+    counter -= 1
 end
 puts "Happy New Year!"
 end
